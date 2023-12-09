877ba6bdd108b87927c9760a7913cff974dec78d
Myck Santos <mick.santos@sergipetec.org.br>
2023-12-09 13:08:17 -0300

    feat: create transactions service into shared folder

---
+++ src/app/shared/data-access/transactions.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { TransactionData } from 'src/app/models/transaction.data';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TransactionsService {
+  private baseURL = '/api';
+
+  constructor(private http: HttpClient) {}
+
+  getTransactions(): Observable<TransactionData> {
+    const endpoint = `${this.baseURL}/transactions`;
+
+    return this.http.get<TransactionData>(endpoint);
+  }
+}

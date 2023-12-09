c41230867ec0f248ddf1a727fc2d640d767bcaf6
Myck Santos <mick.santos@sergipetec.org.br>
2023-12-09 12:50:35 -0300

    feat: create sidebar component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './feature/home.component';
 import { Router, RouterModule } from '@angular/router';
 import { HomeRoutingModule } from './home-routing.module';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, SidebarComponent],
   imports: [CommonModule, HomeRoutingModule],
 })
 export class HomeModule {}

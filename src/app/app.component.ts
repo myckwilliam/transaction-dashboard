c00cf10cdb0eb4186670c591cbf93fc23bda80bf
Myck Santos <mick.santos@sergipetec.org.br>
2023-12-09 13:01:21 -0300

    fix: remove home data-access

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,4 @@ import { TransactionsService } from './home/data-access/services/transactions.se
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
-  constructor(private transactionService: TransactionsService) {}
-
-  ngOnInit(): void {
-    this.transactionService.getTransactions().subscribe(console.log);
-  }
-}
+export class AppComponent {}

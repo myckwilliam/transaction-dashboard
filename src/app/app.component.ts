b0d29b89145126d9be63567046191126b7736fe6
Myck Santos <mick.santos@sergipetec.org.br>
2023-12-11 23:13:51 -0300

    feat: initialize media query implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,18 @@
+import { MediaQueryService } from 'src/app/shared';
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  isDesktop$!: Observable<boolean>;
+
+  constructor(private mediaQueryService: MediaQueryService) {}
+
+  ngOnInit(): void {
+    this.isDesktop$ = this.mediaQueryService.isDesktop$;
+  }
+}

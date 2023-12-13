30f62c60c67b867a5865b51e8282f7939b9a4755
Myck Santos <mick.santos@sergipetec.org.br>
2023-12-13 11:18:55 -0300

    feat: complete responsive navbar and change imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ButtonModule } from 'primeng/button';
+import { SidebarModule } from 'primeng/sidebar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MobileNavbarComponent } from './components/mobile-navbar/mobile-navbar.component';
+import { SidebarContentComponent } from './components/sidebar-content/sidebar-content.component';
 
 @NgModule({
-  declarations: [AppComponent, SidebarComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  declarations: [AppComponent, SidebarComponent, MobileNavbarComponent, SidebarContentComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ButtonModule,
+    SidebarModule,
+    BrowserAnimationsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })

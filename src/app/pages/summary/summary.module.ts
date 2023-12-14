2255ad6b7fea0aa162d4fd0c640a3bbafd5db9cc
Myck Santos <mick.santos@sergipetec.org.br>
2023-12-14 03:43:19 -0300

    feat: create card component and fix layout

--- src/app/pages/summary/summary.module.ts
+++ src/app/pages/summary/summary.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SummaryComponent } from './feature/summary.component';
 import { SummaryRoutingModule } from './summary-routing.module';
+import { SharedModule } from 'src/app/shared';
 
 @NgModule({
   declarations: [SummaryComponent],
-  imports: [CommonModule, SummaryRoutingModule],
+  imports: [CommonModule, SummaryRoutingModule, SharedModule],
 })
 export class SummaryModule {}
--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -1,2 +1,3 @@
 export * from './services/media-query.service';
 export * from './services/transactions.service';
+export * from './shared.module';

e59ee41c8fde9804af1a2a9c646c05f535fdbe60
Myck Santos <mick.santos@sergipetec.org.br>
2023-12-15 04:47:18 -0300

    test: finilize with summary

--- src/app/pages/summary/components/total-cards/total-cards.component.spec.ts
+++ src/app/pages/summary/components/total-cards/total-cards.component.spec.ts
@@ -1,25 +1,43 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { TotalCardsComponent } from './total-cards.component';
+import { Component, Input } from '@angular/core';
+import { TransactionSummary } from 'src/app/models';
+
+@Component({
+  template: `<app-total-cards [summary]="testSummary"></app-total-cards>`,
+})
+class TestHostComponent {
+  testSummary: TransactionSummary = {
+    totalQuantity: 10,
+    totalAmount: 1000,
+    totalNetAmount: 900,
+    totalAverageAmount: 90,
+    initialDate: '2021-05-26',
+    finalDate: '2021-05-26',
+  };
+}
+
+@Component({
+  selector: 'app-card',
+  template: '<ng-content></ng-content>',
+})
+class CardStubComponent {}
 
 describe('TotalCardsComponent', () => {
-  let component: TotalCardsComponent;
-  let fixture: ComponentFixture<TotalCardsComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ TotalCardsComponent ]
-    })
-    .compileComponents();
-  });
+  let fixture: ComponentFixture<TestHostComponent>;
+  let hostComponent: TestHostComponent;
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(TotalCardsComponent);
-    component = fixture.componentInstance;
+    TestBed.configureTestingModule({
+      declarations: [TotalCardsComponent, TestHostComponent, CardStubComponent],
+    });
+
+    fixture = TestBed.createComponent(TestHostComponent);
+    hostComponent = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should create the component', () => {
+    expect(hostComponent).toBeTruthy();
   });
 });

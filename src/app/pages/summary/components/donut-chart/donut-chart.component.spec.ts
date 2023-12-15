3f128af1028c28be78d38b2f40ae9adb20ae7b88
Myck Santos <mick.santos@sergipetec.org.br>
2023-12-15 04:31:54 -0300

    test: implement chart tests

--- src/app/pages/summary/components/donut-chart/donut-chart.component.spec.ts
+++ src/app/pages/summary/components/donut-chart/donut-chart.component.spec.ts
@@ -1,25 +1,107 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DonutChartComponent } from './donut-chart.component';
+import { Component } from '@angular/core';
+import { Transaction } from 'src/app/models';
+
+@Component({
+  template: `<app-donut-chart
+    [transactions]="testTransactions"
+  ></app-donut-chart>`,
+})
+class TestHostComponent {
+  testTransactions: Transaction[] = [
+    {
+      id: '114606514478703',
+      merchantId: '2000463023',
+      paymentNode: 485173,
+      cnpjRoot: 485116,
+      date: '2021-05-26T12:12:55',
+      paymentType: 'Crédito à vista',
+      cardBrand: 'Mastercard',
+      authorizationCode: '378216',
+      truncatedCardNumber: '1014',
+      grossAmount: 80,
+      netAmount: 76.88,
+      terminal: '00032668',
+      administrationFee: 3.9,
+      channelCode: 15,
+      channel: 'Super Link / Digitada',
+      withdrawAmount: 0,
+      minimumMDRAmmount: -3.12,
+      mdrTaxAmount: 0,
+      mdrFeeAmount: -3.12,
+      status: 'Aprovada',
+    },
+    {
+      id: '114606514478704',
+      merchantId: '2000463023',
+      paymentNode: 485173,
+      cnpjRoot: 485116,
+      date: '2021-05-26T12:12:55',
+      paymentType: 'Crédito à vista',
+      cardBrand: 'Mastercard',
+      authorizationCode: '378218',
+      truncatedCardNumber: '1014',
+      grossAmount: 80,
+      netAmount: 76.88,
+      terminal: '00032668',
+      administrationFee: 3.9,
+      channelCode: 15,
+      channel: 'Super Link / Digitada',
+      withdrawAmount: 0,
+      minimumMDRAmmount: -3.12,
+      mdrTaxAmount: 0,
+      mdrFeeAmount: -3.12,
+      status: 'Aprovada',
+    },
+    {
+      id: '114606514478705',
+      merchantId: '2000463023',
+      paymentNode: 485173,
+      cnpjRoot: 485116,
+      date: '2021-05-26T12:12:55',
+      paymentType: 'Crédito à vista',
+      cardBrand: 'Mastercard',
+      authorizationCode: '378224',
+      truncatedCardNumber: '1014',
+      grossAmount: 90,
+      netAmount: 86.49,
+      terminal: '00032668',
+      administrationFee: 3.9,
+      channelCode: 15,
+      channel: 'Super Link / Digitada',
+      withdrawAmount: 0,
+      minimumMDRAmmount: -3.51,
+      mdrTaxAmount: 0,
+      mdrFeeAmount: -3.51,
+      status: 'Aprovada',
+    },
+  ];
+}
 
 describe('DonutChartComponent', () => {
-  let component: DonutChartComponent;
-  let fixture: ComponentFixture<DonutChartComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ DonutChartComponent ]
-    })
-    .compileComponents();
-  });
+  let fixture: ComponentFixture<TestHostComponent>;
+  let donutChartComponent: DonutChartComponent;
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(DonutChartComponent);
-    component = fixture.componentInstance;
+    TestBed.configureTestingModule({
+      declarations: [DonutChartComponent, TestHostComponent],
+    });
+
+    fixture = TestBed.createComponent(TestHostComponent);
+    donutChartComponent = fixture.debugElement.children[0].componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should create the component', () => {
+    expect(donutChartComponent).toBeTruthy();
+  });
+
+  it('should initialize data based on provided transactions', () => {
+    donutChartComponent.ngOnInit();
+    fixture.detectChanges();
+
+    expect(donutChartComponent.data).toBeTruthy();
   });
 });
--- src/app/pages/summary/components/pie-chart/pie-chart.component.spec.ts
+++ src/app/pages/summary/components/pie-chart/pie-chart.component.spec.ts
@@ -1,25 +1,104 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PieChartComponent } from './pie-chart.component';
+import { Component } from '@angular/core';
+import { Transaction } from 'src/app/models';
 
+@Component({
+  template: `<app-pie-chart [transactions]="testTransactions"></app-pie-chart>`,
+})
+class TestHostComponent {
+  testTransactions: Transaction[] = [
+    {
+      id: '114606514478703',
+      merchantId: '2000463023',
+      paymentNode: 485173,
+      cnpjRoot: 485116,
+      date: '2021-05-26T12:12:55',
+      paymentType: 'Crédito à vista',
+      cardBrand: 'Mastercard',
+      authorizationCode: '378216',
+      truncatedCardNumber: '1014',
+      grossAmount: 80,
+      netAmount: 76.88,
+      terminal: '00032668',
+      administrationFee: 3.9,
+      channelCode: 15,
+      channel: 'Super Link / Digitada',
+      withdrawAmount: 0,
+      minimumMDRAmmount: -3.12,
+      mdrTaxAmount: 0,
+      mdrFeeAmount: -3.12,
+      status: 'Aprovada',
+    },
+    {
+      id: '114606514478704',
+      merchantId: '2000463023',
+      paymentNode: 485173,
+      cnpjRoot: 485116,
+      date: '2021-05-26T12:12:55',
+      paymentType: 'Crédito à vista',
+      cardBrand: 'Mastercard',
+      authorizationCode: '378218',
+      truncatedCardNumber: '1014',
+      grossAmount: 80,
+      netAmount: 76.88,
+      terminal: '00032668',
+      administrationFee: 3.9,
+      channelCode: 15,
+      channel: 'Super Link / Digitada',
+      withdrawAmount: 0,
+      minimumMDRAmmount: -3.12,
+      mdrTaxAmount: 0,
+      mdrFeeAmount: -3.12,
+      status: 'Aprovada',
+    },
+    {
+      id: '114606514478705',
+      merchantId: '2000463023',
+      paymentNode: 485173,
+      cnpjRoot: 485116,
+      date: '2021-05-26T12:12:55',
+      paymentType: 'Crédito à vista',
+      cardBrand: 'Mastercard',
+      authorizationCode: '378224',
+      truncatedCardNumber: '1014',
+      grossAmount: 90,
+      netAmount: 86.49,
+      terminal: '00032668',
+      administrationFee: 3.9,
+      channelCode: 15,
+      channel: 'Super Link / Digitada',
+      withdrawAmount: 0,
+      minimumMDRAmmount: -3.51,
+      mdrTaxAmount: 0,
+      mdrFeeAmount: -3.51,
+      status: 'Aprovada',
+    },
+  ];
+}
 describe('PieChartComponent', () => {
-  let component: PieChartComponent;
-  let fixture: ComponentFixture<PieChartComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ PieChartComponent ]
-    })
-    .compileComponents();
-  });
+  let pieChartComponent: PieChartComponent;
+  let fixture: ComponentFixture<TestHostComponent>;
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(PieChartComponent);
-    component = fixture.componentInstance;
+    TestBed.configureTestingModule({
+      declarations: [PieChartComponent, TestHostComponent],
+    });
+
+    fixture = TestBed.createComponent(TestHostComponent);
+    pieChartComponent = fixture.debugElement.children[0].componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should create the component', () => {
+    expect(pieChartComponent).toBeTruthy();
+  });
+
+  it('should initialize data based on provided transactions', () => {
+    pieChartComponent.ngOnInit();
+    fixture.detectChanges();
+
+    expect(pieChartComponent.data).toBeTruthy();
   });
 });

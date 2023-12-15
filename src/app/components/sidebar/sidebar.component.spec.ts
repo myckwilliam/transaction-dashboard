17c168a479782e7394c83e26ef7899d789f3e722
Myck Santos <mick.santos@sergipetec.org.br>
2023-12-15 04:15:22 -0300

    test: implement test for sidebar and card

--- src/app/components/sidebar/sidebar.component.spec.ts
+++ src/app/components/sidebar/sidebar.component.spec.ts
@@ -1,25 +1,37 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SidebarComponent } from './sidebar.component';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'router-link',
+  template: '<ng-content></ng-content>',
+})
+class RouterLinkStub {}
 
 describe('SidebarComponent', () => {
   let component: SidebarComponent;
   let fixture: ComponentFixture<SidebarComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ SidebarComponent ]
-    })
-    .compileComponents();
-  });
-
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [SidebarComponent, RouterLinkStub],
+    });
+
     fixture = TestBed.createComponent(SidebarComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set default height if not provided', () => {
+    expect(component.height).toBe('95vh');
+  });
+
+  it('should set provided height', () => {
+    component.height = '100vh';
+    fixture.detectChanges();
+    expect(component.height).toBe('100vh');
   });
 });
